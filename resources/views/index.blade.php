048b9c3a2ecff10d4a647171a305e4ad86d73bc7
aridwann <aridwansaja84@gmail.com>
2025-07-16 18:30:02 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -94,8 +94,7 @@
                                     <!-- Modal content -->
                                     <div
                                         class="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
-                                        <button type="button" data-modal-target="deleteModal{{ $mhs->id }}"
-                                            data-modal-toggle="deleteModal{{ $mhs->id }}"
+                                        <button type="button"
                                             class="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
                                             data-modal-toggle="deleteModal{{ $mhs['id'] }}">
                                             <svg aria-hidden="true" class="w-5 h-5" fill="currentColor"
@@ -113,18 +112,18 @@
                                                 d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
                                                 clip-rule="evenodd" />
                                         </svg>
-                                        <p class="mb-4 text-gray-500 dark:text-gray-300">Are you sure you want
-                                            to delete this item?</p>
+                                        <p class="mb-4 text-gray-500 dark:text-gray-300">Apakah anda yakin akan
+                                            menghapus data ini?</p>
                                         <div class="flex justify-center items-center space-x-4">
                                             <button data-modal-toggle="deleteModal{{ $mhs['id'] }}" type="button"
-                                                class="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No,
-                                                cancel</button>
+                                                class="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Tidak,
+                                                batal</button>
                                             <form action="/{{ $mhs['id'] }}" method="POST">
                                                 @csrf
                                                 @method('DELETE')
                                                 <button type="submit"
-                                                    class="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">Yes,
-                                                    I'm sure</button>
+                                                    class="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">Ya,
+                                                    saya yakin</button>
                                             </form>
                                         </div>
                                     </div>
